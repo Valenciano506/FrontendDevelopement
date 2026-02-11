235284c3c3aa33530ddc8564cb18c087ce936d43
unknown <valenciano3192006@gmail.com>
2026-02-11 11:33:00 +0100

    link js file

---
+++ HTML/mediaquery/js/timer/timer.js
@@ -0,0 +1,6 @@
+//"document" object represents the whole html page wich is a tree data structure
+//document.getElementById("timerDisplay")
+//this method or command allows us to retrieve an element in our page by its id
+
+const timerDisplay = document.getElementById("timerDisplay");
+console.debug(timerDisplay);
