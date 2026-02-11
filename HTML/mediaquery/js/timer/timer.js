b740120f2dbdb6490df59c15c4f67bc2ef0cf8dc
unknown <valenciano3192006@gmail.com>
2026-02-11 12:59:59 +0100

    animation

--- HTML/mediaquery/js/timer/timer.js
+++ HTML/mediaquery/js/timer/timer.js
@@ -7,3 +7,28 @@ console.debug(timerDisplay);
 const startBtn = document.getElementById("startBtn");
 const stopBtn = document.getElementById("stopBtn");
 const resetBtn = document.getElementById("resetBtn");
+console.log(startBtn);
+console.log(stopBtn);
+console.log(resetBtn);
+//change the font size of the timer label using js
+timerDisplay.style.fontSize = "40px";
+
+startBtn.addEventListener("click", ()=>{console.log("you are clicking me")});
+sizeAnimation();
+
+function sizeAnimation(){
+let size = 40;
+let x = 1000;
+while(x == 0){
+//increment the font size
+for (let i = 0; i < 50; i++){
+    size++;
+    timerDisplay.style.fontSize = size + "px";
+}
+for (let i = 0; i < 50; i++){
+    size--;
+    timerDisplay.style.fontSize = size + "px";
+}
+    x--;
+}
+}
