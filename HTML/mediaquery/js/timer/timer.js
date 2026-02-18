3e588104f6206bf2fd17e180e0e3675f7008759b
unknown <valenciano3192006@gmail.com>
2026-02-18 14:36:51 +0100

    animation

--- HTML/mediaquery/js/timer/timer.js
+++ HTML/mediaquery/js/timer/timer.js
@@ -13,8 +13,48 @@ console.log(resetBtn);
 //change the font size of the timer label using js
 timerDisplay.style.fontSize = "40px";
 
-startBtn.addEventListener("click", ()=>{console.log("you are clicking me")});
-sizeAnimation();
+//Associate the click event with a function,
+//so that each time when the button
+//is clicked, the fimction will be called
+//Anonymous function
+startBtn.addEventListener("click", function(){
+    //What setInterval does is executing a function periodically,
+    //the first parameter is the function that we want to execute,
+    //the second is the period.
+    setInterval(()=>{
+        //1. If the size of the font has reached the maximun, 
+        //it starts to reduce gradually.
+        if(!isGrowing){//If it is not growing
+            fontSize--;
+            timerDisplay.style.fontSize = fontSize + "px";
+            //Check if the size has reached the limit
+            if(fontSize <= minimunSize){
+                isGrowing = true;
+            }
+        }
+        //2. If the size of the font has reached the minimun,
+        //it starts to increase gradually.
+        else{
+            fontSize++;
+            timerDisplay.style.fontSize = fontSize + "px";
+            if(fontSize >= maximunSize){
+                isGrowing = false;
+            }
+        }        
+    }, 1)
+});
+//Arrow function as parameter
+stopBtn.addEventListener("click", ()=>{});
+let fontSize = 40;
+let maximunSize = 200;
+let minimunSize = 20;
+let isGrowing = true;
+//Named function
+function resetTimer(){
+
+}
+resetBtn.addEventListener("click", resetTimer);
+//sizeAnimation();
 
 function sizeAnimation(){
 let size = 40;
