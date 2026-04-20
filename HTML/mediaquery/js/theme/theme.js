2d5127c110e13a020592a230e668ce2b24610aae
unknown <valenciano3192006@gmail.com>
2026-04-20 12:41:48 +0200

    javascript task 5

---
+++ HTML/mediaquery/js/theme/theme.js
@@ -0,0 +1,17 @@
+//retrieve all the theme buttons using class selector
+const btns = document.querySelectorAll(".theme-btn");
+//obtain the body object
+const bd = document.querySelector("body");
+console.log(bd);
+console.log(btns);
+//iterate all theme buttons to associate each one with 
+//a task that we want to execute when ocurring a click event
+for (let index = 0; index < btns.length; index++){
+    const btn = btns[index];
+    console.log(btn);
+    btn.addEventListener("click", ()=>{
+        console.log(btn.getAttribute("data-theme"));
+        console.log(btn.getAttribute("class"));
+        bd.className = btn.getAttribute("data-theme");
+    });
+}
