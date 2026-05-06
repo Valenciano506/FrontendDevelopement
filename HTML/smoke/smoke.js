0060b8c0911433de3dd25e8cf5113e0d180f2e39
unknown <valenciano3192006@gmail.com>
2026-05-06 13:17:45 +0200

    smoke.js

--- HTML/smoke/smoke.js
+++ HTML/smoke/smoke.js
@@ -1,18 +1,18 @@
-const min_particle_size = 0.2;
+const min_particle_size = 10;
 const min_speed_x = 1;
 const min_speed_y = 1;
+const num_of_particles = 10;
 const canvas = document.getElementById("smoke_canvas");
 console.log(canvas);
 const ctx = canvas.getContext("2d");
 //Define the color that we want to use for our drawings
-ctx.fillStyle = 'rgb(0, 0, 0)';
+ctx.fillStyle = 'rgb(255, 0, 0)';
 //Prepare to draw a path
 ctx.beginPath();
 //Draw an arc
-ctx.arc(20, 30, 20, 0, Math.PI * 2);
+ctx.arc(2, 3, 2, 0, Math.PI * 2);
 //Fill the object that we have drawn
 ctx.fill();
-console.log(smoke);
 
 class Smoke{
     constructor(){
@@ -25,11 +25,16 @@ class Smoke{
     }
 
     animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (let index = 0; index < this.particles.length; index++) {
             //Update the states of the particle and draw it to the screen
-            this.particles[index].update();            
+            this.particles[index].update();
+            if (this.particles[index].size <= 0.2){
+                this.particles.splice(index, 1);
+                index--;
+            }
         }
-        requestAnimationFrame(animate);
+        requestAnimationFrame(() => this.animate());
     }
 
 }
@@ -37,16 +42,16 @@ class Smoke{
 class Particles{
     constructor(x, y){
         this.size = Math.random() * 2 + min_particle_size;
-        this.color = '#23a';
+        this.color = 'rgb(255, 0, 0)';
         this.speedX = Math.random() * 2 + min_speed_x;
         this.speedY = Math.random() * 2 + min_speed_y;
-        this.X = Math.random() * 2 + x;
-        this.Y = Math.random() * 2 + y;
+        this.x = Math.random() * 2 + x;
+        this.y = Math.random() * 2 + y;
     }
 
     //Update the current state of a particle
     update(){
-        this.size -= 1;
+        this.size -= 0.1;
         //this.size = this.size - 1;
 
         this.draw();
@@ -68,13 +73,13 @@ class Particles{
 //Create a smoke object
 const smoke = new Smoke();
 console.log(smoke);
-window.addEventListener('mousedown', (e)=>{
+window.addEventListener('mousemove', (e)=>{
     console.log(e);
     console.log(e.clientX);
     console.log(e.clientY);
 
     //Create particles and add them to the smoke object
-    for (let index = 0; index < min_particle_size; index++) {
+    for (let index = 0; index < num_of_particles; index++) {
         const particle = new Particles(e.clientX, e.clientY);
         smoke.add(particle);
         console.log(smoke.particles);
